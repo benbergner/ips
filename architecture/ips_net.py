e1581198d7ed25340d0f0e2d121b608db799482e
Benjamin <Benjamin>
2022-10-02 13:16:03 +0200

    added IPS and transformer

---
+++ architecture/ips_net.py
@@ -0,0 +1,182 @@
+import math
+
+import torch
+import torch.nn as nn
+from torchvision.models import resnet18, resnet50
+
+from architecture.transformer import Transformer, pos_enc_1d
+
+class IPSNet(nn.Module):
+    ''' Net that runs IPS, patch encoder, aggregator, class. head '''
+
+    def get_patch_enc(self, enc_type, pretrained, n_chan_in, n_res_blocks):
+
+        # get architecture for patch encoder
+        if enc_type == 'resnet18': 
+            res_net_fn = resnet18
+        elif enc_type == 'resnet50':
+            res_net_fn = resnet50
+        res_net = res_net_fn(pretrained=pretrained)
+
+        if n_chan_in == 1:
+            # standard resnet uses 3 input channels
+            res_net.conv1 = nn.Conv2d(n_chan_in, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        
+        # compose patch encoder
+        layer_ls = []
+        layer_ls.extend([
+            res_net.conv1,
+            res_net.bn1,
+            res_net.relu,
+            res_net.maxpool,
+            res_net.layer1,
+            res_net.layer2
+        ])
+
+        if n_res_blocks == 4:
+            layer_ls.extend([
+                res_net.layer3,
+                res_net.layer4,
+                res_net.avgpool
+            ])
+
+        return nn.Sequential(layer_ls)
+
+    def __init__(self, n_class, use_patch_enc, enc_type, pretrained, n_chan_in, n_res_blocks,
+        use_pos, n_tasks, N, M, I, D, H, D_k, D_v, D_inner, dropout, attn_dropout, device):#n_class, n_channel, hw, mem_size, rand_size, iter_size, n_backbone_layer, n_layer, n_token, n_head, d_k, d_v, d_model, d_inner, attn_dropout, dropout, device, use_pretrained=True, outp_act_fn='softmax', use_local_pred=False
+        super().__init__()
+
+        self.use_pos = use_pos
+        self.M = M
+        self.I = I
+        self.D = D 
+
+        if use_patch_enc:
+            self.patch_encoder = self.get_patch_enc(enc_type, pretrained, n_chan_in, n_res_blocks))
+
+        # define the multi-head cross-attention transformer
+        self.transf = Transformer(n_token, H, D, D_k, D_v, D_inner, attn_dropout, dropout)
+
+        if use_pos:
+            self.pos_enc = pos_enc_1d(D, N).unsqueeze(0).to(device)
+        
+        # define output layer(s)
+        self.outp_layers = nn.ModuleList([nn.Linear(D, C) for i in range(n_tasks)])
+
+    def IPS(self, patches):
+        
+        # get useful stuff
+        M = self.M
+        I = self.I
+        D = self.D  
+        device = self.device
+        use_pos = self.use_pos
+        patch_shape = patches.shape
+        B, N = patch_shape[:2]
+
+        # IPS required?
+        if M >= N:
+            return patches.to(device)      
+
+        # should patch encoder be used?
+        if len(patch_shape) == 3: # B, N, D
+            is_image = False
+        elif len(patches.shape) == 5: # B, N, n_chan_in, height, width
+            is_image = True
+        else:
+            raise ValueError('The input is neither an image (5 dim) nor a feature vector (3 dim).')
+        
+        # IPS runs in no-gradient mode
+        with torch.no_grad():
+            # IPS runs in evaluation mode
+            if self.training:
+                self.patch_encoder.eval()
+                self.transf.eval()
+            
+            # adjust positional encoding to batch
+            if use_pos:
+                pos_enc = self.pos_enc.repeat(B, 1, 1)
+
+            # init memory
+            init_patch = patches[:,:M].to(device)
+            if is_image:
+                mem_emb = self.patch_encoder(init_patch.reshape(-1, *patch_shape.shape[2:]))
+                mem_emb = mem_emb.view(B, M, -1)
+            else:
+                mem_emb = init_patch
+            
+            # init mem idx
+            idx = torch.arange(N, dtype=torch.int64, device=device).unsqueeze(0).repeat(B, 1)
+            mem_idx = idx[:,:M]
+
+            # get num iterations
+            n_iter = math.ceil((N - M) / I)
+            for i in range(n_iter):
+                # get next patches
+                start_idx = i * I + M
+                end_idx = min(start_idx + I, N)
+
+                iter_patch = patches[:, start_idx:end_idx].to(device)
+                iter_idx = idx[:, start_idx:end_idx]
+                if use_pos:
+                    iter_pos_enc = pos_enc[:, start_idx:end_idx]
+
+                # embed patches
+                if is_image:
+                    iter_emb = self.patch_encoder(iter_patch.reshape(-1, *patch_shape.shape[2:]))
+                    iter_emb = iter_emb.view(B, -1, D)
+                else:
+                    iter_emb = iter_patch
+                
+                # concatenate with memory buffer
+                all_emb = torch.cat(mem_emb, iter_emb)
+                all_idx = torch.cat((mem_idx, iter_idx), dim=-1)
+                if use_pos:
+                    all_emb_pos = iter_emb + iter_pos_enc
+                else:
+                    all_emb_pos = None
+
+                mem_emb, mem_idx = self.select_feat(all_emb, all_idx, all_emb_pos)#feat_iter_pos
+            
+            # select patches
+            mem_patch = torch.gather(patches, 1, 
+                mem_idx.view(B, -1, torch.ones(patch_shape[2:])).repeat(1, 1, *patch_shape[2:]).to(patches.device)
+            )
+            if use_pos:
+                mem_pos = torch.gather(pos_enc, 1, mem_idx.unsqueeze(-1).repeat(1, 1, D))
+            else:
+                mem_pos = None
+
+            # set network back to gradient and training mode            
+            if self.training:
+                self.patch_encoder.train()
+                self.transf.train()
+    
+    return mem_patch.to(device), mem_pos
+
+    def forward(self, mem_patch, mem_pos):
+        patch_shape = mem_patch.shape
+        B, M = patch_shape.shape[:2]
+
+        if len(patch_shape) == 3: # B, N, D
+            is_image = False
+        elif len(patches.shape) == 5: # B, N, n_chan_in, height, width
+            is_image = True
+        else:
+            raise ValueError('The input is neither an image (5 dim) nor a feature vector (3 dim).')
+        
+        if is_image:
+            mem_emb = self.patch_encoder(mem_patch.reshape(-1, *patch_shape.shape[2:]))
+            mem_emb = mem_emb.view(B, M, -1)        
+
+        if torch.is_tensor(mem_pos):
+            mem_emb = mem_emb + mem_pos
+
+        image_emb = self.transf(mem_emb, return_attns=False)
+
+        preds = []
+        for i, layer in enumerate(self.outp_layers):
+            pred = layer(image_emb[:,i]) # .view(b, -1)
+            preds.append(pred)
+ 
+        return preds
---
+++ architecture/transformer.py
@@ -0,0 +1,124 @@
+def pos_enc_1d(D, seq_len):
+    
+    if D % 2 != 0:
+        raise ValueError("Cannot use sin/cos positional encoding with "
+                         "odd dim (got dim={:d})".format(D))
+    pe = torch.zeros(seq_len, D)
+    position = torch.arange(0, seq_len).unsqueeze(1)
+    div_term = torch.exp((torch.arange(0, D, 2, dtype=torch.float) *
+                         -(math.log(10000.0) / D)))
+    pe[:, 0::2] = torch.sin(position.float() * div_term)
+    pe[:, 1::2] = torch.cos(position.float() * div_term)
+
+    return pe
+
+class DotProductAttention(nn.Module):
+    ''' Dot-Product Attention '''
+
+    def __init__(self, temperature, attn_dropout=0.1):
+        super().__init__()
+
+        self.temperature = temperature
+        self.dropout = nn.Dropout(attn_dropout)
+
+    def forward(self, q, k, v):
+        attn = torch.matmul(q / self.temperature, k.transpose(2, 3))
+        attn = self.dropout(torch.softmax(attn, dim=-1))
+        output = torch.matmul(attn, v)
+
+        return output, attn
+
+class MultiHeadCrossAttention(nn.Module):
+    ''' Multi-head cross-attention module '''
+
+    def __init__(self, n_token, H, D, D_k, D_v, attn_dropout=0.1, dropout=0.1):
+        super().__init__()
+
+        self.n_token = n_token
+        self.H = H
+        self.D_k = D_k
+        self.D_v = D_v
+
+        self.q = nn.Parameter(torch.empty((1, n_token, D)))
+        q_init_val = math.sqrt(1 / D_k)
+        nn.init.uniform_(self.q, a=-q_init_val, b=q_init_val)
+
+        self.q_w = nn.Linear(D, H * D_k, bias=False)
+        self.k_w = nn.Linear(D, H * D_k, bias=False)
+        self.v_w = nn.Linear(D, H * D_v, bias=False)
+        self.fc = nn.Linear(H * D_v, D, bias=False)
+
+        self.attention = DotProductAttention(
+            temperature=D_k ** 0.5,
+            attn_dropout=attn_dropout
+        )
+
+        self.dropout = nn.Dropout(dropout)
+        self.layer_norm = nn.LayerNorm(D, eps=1e-6)
+
+    def forward(self, x):
+
+        D_k, D_v, H, n_token = self.D_k, self.D_v, self.H, self.n_token
+        B, seq_len = x.shape[:2]
+
+        # project and separate heads
+        q = self.q_w(self.q).view(1, n_token, H, D_k)
+        k = self.k_w(x).view(B, seq_len, H, D_k)
+        v = self.v_w(x).view(B, seq_len, H, D_v)
+
+        # transpose for attention dot product: B x H x seq_len x D_k or D_v
+        q, k, v = q.transpose(1, 2), k.transpose(1, 2), v.transpose(1, 2)
+        # cross-attention
+        x, attn = self.attention(q, k, v)
+
+        # transpose again: B x n_token x H x D_v
+        # concat heads: B x n_token x (H * D_v)
+        x = x.transpose(1, 2).contiguous().view(b, n_token, -1)
+        # combine heads
+        x = self.dropout(self.fc(x))
+        # residual connection + layernorm
+        x += self.q
+        x = self.layer_norm(x)
+
+        return x, attn
+
+class MLP(nn.Module):
+    ''' MLP consisting of two feed-forward layers '''
+
+    def __init__(self, D, D_inner, dropout=0.1):
+        super().__init__()
+
+        self.w_1 = nn.Linear(D, D_inner)
+        self.w_2 = nn.Linear(D_inner, D)
+        self.layer_norm = nn.LayerNorm(D, eps=1e-6)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+
+        residual = x
+
+        x = self.w_2(torch.relu(self.w_1(x)))
+        x = self.dropout(x)
+        
+        x += residual
+        x = self.layer_norm(x)
+
+        return x
+
+class Transformer(nn.Module):
+    """ Cross-attention based transformer module """
+
+    def __init__(self, n_token, H, D, D_k, D_v, D_inner, attn_dropout=0.1, dropout=0.1):
+        super().__init__()
+
+        self.crs_attn = MultiHeadCrossAttention(n_token, H, D, D_k, D_v, attn_dropout=attn_dropout, dropout=dropout)
+        self.mlp = MLP(D, D_inner, dropout=dropout)
+    
+    def forward(self, x, return_attns=False):
+
+        x, attn = self.crs_attn(x)
+        x = self.mlp(x)
+
+        if return_attns:
+            return x, attn
+        return x
